eb101192a4460f7be12441e22a73bcd8e84db536
BrendenKingTLG <kingbren@oregonstate.edu>
2024-02-13 00:47:06 -0600

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "icon-green":"#53d769"
+    },
     },
   },
   plugins: [],
